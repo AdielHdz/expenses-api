2d4627ba10513d85d4abb9fa7f1eafb6b5ef8e29
Adiel Hernandez <wereber.dragons14@live.com.mx>
2026-01-18 18:41:48 -0600

    chore(config): separate data-source.ts & seeders config

---
+++ src/SeederConfig.ts
@@ -0,0 +1,5 @@
+// Configuración de seeders (solo para typeorm-extension)
+export const SeederConfig = {
+  seeds: ['src/database/seeds/**/*{.ts,.js}'],
+  factories: ['src/database/factories/**/*{.ts,.js}'],
+};
--- src/data-source.ts
+++ src/data-source.ts
@@ -2,21 +2,22 @@ import 'dotenv/config';
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 
-// DataSource para TypeORM (migraciones y NestJS)
 export const AppDataSource = new DataSource({
-  type: 'postgres', // literal 'postgres'
-  host: process.env.PGHOST,
+  type: 'postgres', // literal
+  host: process.env.PGHOST, // ! asegura que no es undefined
   port: +process.env.PGPORT,
   username: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/database/migrations/*.ts'],
-  synchronize: false, // nunca true en producción
+  entities: [
+    process.env.ENVIRONMENT === 'production'
+      ? 'dist/**/*.entity.js'
+      : 'src/**/*.entity.ts',
+  ], // usa dist para build
+  migrations: [
+    process.env.ENVIRONMENT === 'production'
+      ? 'dist/database/migrations/*.js'
+      : 'src/database/migrations/*.ts',
+  ], // usa dist para build
+  synchronize: false,
 });
-
-// Configuración de seeders (solo para typeorm-extension)
-export const SeederConfig = {
-  seeds: ['src/database/seeds/**/*{.ts,.js}'],
-  factories: ['src/database/factories/**/*{.ts,.js}'],
-};
