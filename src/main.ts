6d6e96a2480ce8b15f4f757af25446b06923a7dd
Adiel Hernandez <wereber.dragons14@live.com.mx>
2026-01-18 19:39:14 -0600

    fix(logs): add variables logs

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
+  console.log('DATABASE_URL:', process.env.DATABASE_URL); // <<-- aquí
+  console.log('ENVIRONMENT:', process.env.ENVIRONMENT);
+
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.CLIENT_ORIGIN,
